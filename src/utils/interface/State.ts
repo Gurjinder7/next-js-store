1e4d5f8fd3e2c1a197feb59efd80d35efc658b31
Gurjinder7 <dev.gurjinder@gmail.com>
2026-01-30 21:46:20 +0000

    persistent zustand

--- src/utils/interface/State.ts
+++ src/utils/interface/State.ts
@@ -1,9 +1,14 @@
 import {IProduct} from "@/utils/interface/product";
 
-export interface IState {
+export type GameStoreState = {
     products: IProduct[];
     authenticated: boolean;
+    user:null;
+    loginDialog: boolean;
     setAuthenticated: (b: boolean) => void;
     addProduct: (product: IProduct) => void;
     removeProduct: (product: IProduct) => void;
+    toggleLoginDialog: (status:boolean) => void;
+    setUser: (user: any) => void;
+    clearData: () => void;
 }
--- store.ts
+++ store.ts
@@ -1,27 +1,57 @@
 import { create } from "zustand"
 import {IProduct} from "@/utils/interface/product";
-import {IState} from "@/utils/interface/State";
-
-const useAppStore = create((set) => ({
-    products:[],
-    authenticated: false,
-    user: null,
-    loginDialog: false,
-    setAuthenticated: (status: boolean) => set((state:IState) => ({authenticated: status})),
-    addProduct: (product: IProduct) => {
-        console.log(product);
-
-        set((state:IState) => ({products: [...state.products, product]}));
-
-    },
-    removeProduct: (product: IProduct) => {
-        set((state:IState) => ({products: state.products.filter((i) => i.id !== product.id)}));
-    },
-    toggleLoginDialog: (status:boolean) => set((state:IState) => ({loginDialog: status})),
-    setUser: (user) => set((state:IState) => ({user: user})),
-    clearData: () => set((state:IState) => ({user: null, products: [], authenticated: false})),
+import {GameStoreState} from "@/utils/interface/State";
+import {persist, createJSONStorage} from "zustand/middleware";
 
 
-}));
+const useAppStore = create<GameStoreState>()(
+    persist(
+        (set, get) => ({
+            products:[],
+            authenticated: false,
+            user: null,
+            loginDialog: false,
+            setAuthenticated: (status: boolean) => set((state:GameStoreState) => ({authenticated: status})),
+            addProduct: (product: IProduct) => {
+                console.log(product);
+
+                set((state:GameStoreState) => ({products: [...state.products, product]}));
+
+            },
+            removeProduct: (product: IProduct) => {
+                set((state:GameStoreState) => ({products: state.products.filter((i) => i.id !== product.id)}));
+            },
+            toggleLoginDialog: (status:boolean) => set((state:GameStoreState) => ({loginDialog: status})),
+            setUser: (user) => set((state:GameStoreState) => ({user: user})),
+            clearData: () => set((state:GameStoreState) => ({user: null, products: [], authenticated: false})),
+        }),
+        {
+            name: 'game-store-storage',
+            storage: createJSONStorage(() => sessionStorage),
+        }
+    )
+)
+
+// const useAppStore = create((set) => ({
+//     products:[],
+//     authenticated: false,
+//     user: null,
+//     loginDialog: false,
+//     setAuthenticated: (status: boolean) => set((state:IState) => ({authenticated: status})),
+//     addProduct: (product: IProduct) => {
+//         console.log(product);
+//
+//         set((state:IState) => ({products: [...state.products, product]}));
+//
+//     },
+//     removeProduct: (product: IProduct) => {
+//         set((state:IState) => ({products: state.products.filter((i) => i.id !== product.id)}));
+//     },
+//     toggleLoginDialog: (status:boolean) => set((state:IState) => ({loginDialog: status})),
+//     setUser: (user) => set((state:IState) => ({user: user})),
+//     clearData: () => set((state:IState) => ({user: null, products: [], authenticated: false})),
+//
+//
+// }));
 
 export default useAppStore;
